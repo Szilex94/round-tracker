f55f851ead6fe1bfcdbb717eb4dab9f560d20b95
szilard.mate <szilard.mate@treedev.site>
2021-06-26 21:52:34 +0300

    More Fixes

--- src/test/java/com/github/szilex94/edu/round_tracker/RoundTrackerApplicationTests.java
+++ src/test/java/com/github/szilex94/edu/round_tracker/RoundTrackerApplicationTests.java
@@ -2,14 +2,12 @@ package com.github.szilex94.edu.round_tracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
-@ContextConfiguration
-class RoundTrackerApplicationTests {
+@SpringBootTest(classes = RoundTrackerApplication.class)
+public class RoundTrackerApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
