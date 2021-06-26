3473fbe43298ddd9250219f0048e166ef541a5ff
szilard.mate <szilard.mate@treedev.site>
2021-06-26 21:42:33 +0300

    Fixed Unit test packages

--- src/test/java/com/github/szilex94/edu/round_tracker/RoundTrackerApplicationTests.java
+++ src/test/java/com/github/szilex94/edu/round_tracker/RoundTrackerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.github.szilex94.edu.roundtracker;
+package com.github.szilex94.edu.round_tracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
