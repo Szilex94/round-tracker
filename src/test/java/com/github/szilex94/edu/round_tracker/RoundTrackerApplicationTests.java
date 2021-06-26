ca2fa900e9f048fc506306220c8b68fff7f6ebbc
szilard.mate <szilard.mate@treedev.site>
2021-06-26 21:54:40 +0300

    Solved Context Load Tests

--- src/test/java/com/github/szilex94/edu/round_tracker/RoundTrackerApplicationTests.java
+++ src/test/java/com/github/szilex94/edu/round_tracker/RoundTrackerApplicationTests.java
@@ -3,7 +3,7 @@ package com.github.szilex94.edu.round_tracker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = RoundTrackerApplication.class)
+@SpringBootTest
 public class RoundTrackerApplicationTests {
 
 	@Test
