a95e3a1a1a283adea931f877fff2b0119e2cfbb3
szilard.mate <szilard.mate@treedev.site>
2021-06-26 21:12:53 +0300

    Initial Project Files

---
+++ src/test/java/com/github/szilex94/edu/roundtracker/RoundTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.szilex94.edu.roundtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoundTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
