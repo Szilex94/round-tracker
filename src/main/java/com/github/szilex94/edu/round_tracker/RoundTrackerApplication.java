96633500087478c5aa87d71c4353d25145e33b5e
Mate Szilard <32964548+Szilex94@users.noreply.github.com>
2022-08-30 00:05:08 +0300

    A bit of code cleanup.

--- src/main/java/com/github/szilex94/edu/round_tracker/RoundTrackerApplication.java
+++ src/main/java/com/github/szilex94/edu/round_tracker/RoundTrackerApplication.java
@@ -1,18 +1,14 @@
 package com.github.szilex94.edu.round_tracker;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@ComponentScan("com.github.szilex94.edu.round_tracker")
-@EnableAutoConfiguration
 public class RoundTrackerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RoundTrackerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RoundTrackerApplication.class, args);
+    }
 
 }
