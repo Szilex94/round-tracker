17dc1a206924e567e2c9385d3aaaebcc877ed645
Mate Szilard <32964548+Szilex94@users.noreply.github.com>
2022-09-27 01:12:16 +0300

    Simplified user-profile service implementation

--- src/main/java/com/github/szilex94/edu/round_tracker/service/user/profile/UserProfileServiceImpl.java
+++ src/main/java/com/github/szilex94/edu/round_tracker/service/user/profile/UserProfileServiceImpl.java
@@ -1,8 +1,5 @@
 package com.github.szilex94.edu.round_tracker.service.user.profile;
 
-import com.github.szilex94.edu.round_tracker.mappers.UserProfileMapper;
-import com.github.szilex94.edu.round_tracker.repository.user.profile.UserProfileDao;
-import com.github.szilex94.edu.round_tracker.repository.user.profile.UserProfileRepository;
 import com.github.szilex94.edu.round_tracker.repository.user.profile.UserProfileRepositoryAdapter;
 import com.github.szilex94.edu.round_tracker.service.user.profile.support.UserProfileUpdateMapper;
 import org.springframework.stereotype.Service;
@@ -14,26 +11,13 @@ import static com.google.common.base.Strings.isNullOrEmpty;
 @Service
 public class UserProfileServiceImpl implements UserProfileService {
 
-    @Deprecated
-    private final UserProfileMapper mapper;
-
-    /**
-     * @deprecated - this functionality will be extracted into a DB adapter
-     */
-    @Deprecated
-    private final UserProfileRepository repository;
-
     private final UserProfileRepositoryAdapter repositoryAdapter;
 
     private final UserProfileUpdateMapper updateMapper;
 
     public UserProfileServiceImpl(UserProfileRepositoryAdapter repositoryAdapter,
-                                  UserProfileMapper mapper,
-                                  UserProfileRepository repository,
                                   UserProfileUpdateMapper updateMapper) {
         this.repositoryAdapter = repositoryAdapter;
-        this.mapper = mapper;
-        this.repository = repository;
         this.updateMapper = updateMapper;
     }
 
@@ -54,16 +38,9 @@ public class UserProfileServiceImpl implements UserProfileService {
         var id = reference.getId();
         checkArgument(!isNullOrEmpty(id), "Null or empty reference id not allowed!");
 
-        return repository.findById(id)
-                .map(db -> updateUserProfile(db, reference))
-                .flatMap(repository::save)
-                .map(mapper::fromDao);
-    }
-
-    private UserProfileDao updateUserProfile(UserProfileDao dbResult, UserProfile desired) {
-        var inDB = mapper.fromDao(dbResult);
-        updateMapper.updateNonNull(desired, inDB);
-        return mapper.toDao(inDB);
+        return repositoryAdapter.findById(id)
+                .map(db -> updateMapper.updateNonNull(reference, db))
+                .flatMap(repositoryAdapter::createOrUpdate);
     }
 
 }
