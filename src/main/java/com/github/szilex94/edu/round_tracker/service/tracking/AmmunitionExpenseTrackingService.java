6fa2ec599514c7366311f9b803889fccd1910efe
Mate Szilard <32964548+Szilex94@users.noreply.github.com>
2022-12-10 01:05:42 +0200

    RT-7 Test Jetbrains Annotations

--- src/main/java/com/github/szilex94/edu/round_tracker/service/tracking/AmmunitionExpenseTrackingService.java
+++ src/main/java/com/github/szilex94/edu/round_tracker/service/tracking/AmmunitionExpenseTrackingService.java
@@ -2,7 +2,6 @@ package com.github.szilex94.edu.round_tracker.service.tracking;
 
 import com.github.szilex94.edu.round_tracker.service.tracking.model.AmmunitionChange;
 import com.github.szilex94.edu.round_tracker.service.tracking.model.UserAmmunitionSummary;
-import org.jetbrains.annotations.TestOnly;
 import reactor.core.publisher.Mono;
 
 /**
@@ -16,6 +15,5 @@ public interface AmmunitionExpenseTrackingService {
      * @param change - represents the change which occurred to a users disposable ammunition count
      * @return a summary containing the current ammunition count
      */
-    @TestOnly
     Mono<UserAmmunitionSummary> recordAmmunitionChange(AmmunitionChange change);
 }
