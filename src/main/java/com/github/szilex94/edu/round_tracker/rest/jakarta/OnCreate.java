a06dba91cf48475cc01e11c1442d93ad8c80885e
Mate Szilard <32964548+Szilex94@users.noreply.github.com>
2023-03-28 00:36:37 +0300

    RT-7 Added marker interfaces meant to be used for grouping javax annotations

---
+++ src/main/java/com/github/szilex94/edu/round_tracker/rest/jakarta/OnCreate.java
@@ -0,0 +1,11 @@
+package com.github.szilex94.edu.round_tracker.rest.jakarta;
+
+/**
+ * Marker Interface meant to be used for grouping javax validations meant to be
+ * executed during the creation phase of an entity
+ *
+ * @author szilex94
+ */
+public interface OnCreate {
+    //Marker Interface
+}
---
+++ src/main/java/com/github/szilex94/edu/round_tracker/rest/jakarta/OnUpdate.java
@@ -0,0 +1,11 @@
+package com.github.szilex94.edu.round_tracker.rest.jakarta;
+
+/**
+ * Marker Interface meant to be used for grouping javax validations meant to be
+ * executed during the update phase of an entity
+ *
+ * @author szilex94
+ */
+public interface OnUpdate {
+    //Marker Interface
+}
