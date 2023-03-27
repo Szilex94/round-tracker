650a2baa6eda2698d7f2e64be3492cd528323cf7
Mate Szilard <32964548+Szilex94@users.noreply.github.com>
2023-03-28 00:30:56 +0300

    RT-7 Added REST API for defining custom ammunition calibers

---
+++ src/main/java/com/github/szilex94/edu/round_tracker/rest/support/SupportController.java
@@ -0,0 +1,42 @@
+package com.github.szilex94.edu.round_tracker.rest.support;
+
+import com.github.szilex94.edu.round_tracker.rest.jakarta.OnCreate;
+import com.github.szilex94.edu.round_tracker.rest.jakarta.OnUpdate;
+import jakarta.validation.Valid;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.APPLICATION_NDJSON_VALUE;
+
+/**
+ * Encompasses support operations like managing supported calibers or other configurations required by the application
+ *
+ * @author szilex94
+ */
+@RestController
+@RequestMapping("round-tracker/v1/support")
+@Validated
+public class SupportController {
+
+
+    @GetMapping(path = "caliberDefinition", produces = APPLICATION_NDJSON_VALUE)
+    public Flux<CaliberTypeDefinitionDto> getSupportedCalibers() {
+        throw new UnsupportedOperationException("TBD");
+    }
+
+    @PostMapping(path = "caliberDefinition", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    @Validated(OnCreate.class)
+    public Mono<CaliberTypeDefinitionDto> addCaliberDefinition(@Valid @RequestBody CaliberTypeDefinitionDto dto) {
+        throw new UnsupportedOperationException("TBD");
+    }
+
+    @PatchMapping(path = "caliberDefinition", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    @Validated(OnUpdate.class)
+    public Mono<CaliberTypeDefinitionDto> patchCaliberDefinition(@Valid @RequestBody CaliberTypeDefinitionDto dto) {
+        throw new UnsupportedOperationException("TBD");
+    }
+
+}
