496a75bc23b2486f7b7c3e7109558bddf0c3d994
Mate Szilard <32964548+Szilex94@users.noreply.github.com>
2023-04-18 22:55:41 +0300

    RT-7 Added missing error code

--- src/main/java/com/github/szilex94/edu/round_tracker/rest/error/ApiErrorCode.java
+++ src/main/java/com/github/szilex94/edu/round_tracker/rest/error/ApiErrorCode.java
@@ -5,6 +5,7 @@ package com.github.szilex94.edu.round_tracker.rest.error;
  */
 public enum ApiErrorCode {
     SYSTEM_API_NOT_SUPPORTED("sys-0000"),
+    CALIBER_DEFINITION_CODE_CONFLICT("cd-0000"),
     USER_PROFILE_UNIQUE_IDENTIFIER_CONFLICT("up-0000");
 
     private final String code;
--- src/main/java/com/github/szilex94/edu/round_tracker/rest/error/GlobalExceptionHandler.java
+++ src/main/java/com/github/szilex94/edu/round_tracker/rest/error/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.github.szilex94.edu.round_tracker.rest.error;
 
 import com.github.szilex94.edu.round_tracker.repository.exception.DuplicateUserProfileException;
+import com.github.szilex94.edu.round_tracker.service.support.caliber.DuplicateCaliberCodeException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,8 +12,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import java.time.OffsetDateTime;
 
-import static com.github.szilex94.edu.round_tracker.rest.error.ApiErrorCode.SYSTEM_API_NOT_SUPPORTED;
-import static com.github.szilex94.edu.round_tracker.rest.error.ApiErrorCode.USER_PROFILE_UNIQUE_IDENTIFIER_CONFLICT;
+import static com.github.szilex94.edu.round_tracker.rest.error.ApiErrorCode.*;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -25,6 +25,24 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
         var body = new GenericErrorResponse()
                 .setApiErrorCode(USER_PROFILE_UNIQUE_IDENTIFIER_CONFLICT.getCode())
+                .setOccurred(OffsetDateTime.now())
+                .setMessage(ex.getMessage());
+
+        return handleExceptionInternal(ex,
+                body,
+                new HttpHeaders(),
+                HttpStatus.CONFLICT,
+                request);
+    }
+
+    @ExceptionHandler(value = DuplicateCaliberCodeException.class)
+    protected ResponseEntity<Object> handleDuplicateCaliberCode(
+            RuntimeException ex,
+            WebRequest request) {
+
+
+        var body = new GenericErrorResponse()
+                .setApiErrorCode(CALIBER_DEFINITION_CODE_CONFLICT.getCode())
                 .setOccurred(OffsetDateTime.now())
                 .setMessage(ex.getMessage());
 
