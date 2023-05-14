a6336948050153f300096b5ad066642483cc4a59
Mate Szilard <32964548+Szilex94@users.noreply.github.com>
2023-05-14 18:49:45 +0300

    RT-20 worked out state transitions and desired actions

--- src/main/java/com/github/szilex94/edu/round_tracker/service/datamanagement/fsm/config/ArchivingFSMConfiguration.java
+++ src/main/java/com/github/szilex94/edu/round_tracker/service/datamanagement/fsm/config/ArchivingFSMConfiguration.java
@@ -5,8 +5,11 @@ import com.github.szilex94.edu.round_tracker.service.datamanagement.fsm.event.Da
 import com.github.szilex94.edu.round_tracker.service.datamanagement.fsm.state.ArchivingState;
 import com.github.szilex94.edu.round_tracker.service.datamanagement.fsm.state.DataManagementState;
 import com.github.szilex94.edu.round_tracker.service.datamanagement.fsm.state.GenericState;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.action.Action;
 import org.springframework.statemachine.config.EnableStateMachine;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
@@ -31,19 +34,40 @@ public class ArchivingFSMConfiguration extends StateMachineConfigurerAdapter<Dat
 
         states.withStates()
                 .initial(GenericState.IDLE)
+                .stateDo(ArchivingState.MARK_ENTITIES, new SimpleAction("MARK"))
+                .stateDo(ArchivingState.TRANSFER, new SimpleAction("TRANSFER"))
+                .stateDo(ArchivingState.CLEANUP, new SimpleAction("CLEANUP"))
                 .states(DataManagementState.allStates());
     }
 
     @Override
     public void configure(StateMachineTransitionConfigurer<DataManagementState, DataManagementEvent> transitions)
             throws Exception {
-        //TODO finish configuration
+
         transitions
                 .withExternal()
-                .source(GenericState.IDLE).target(ArchivingState.MARK_ENTITIES).event(ArchivingEvent.ARCHIVING_START);
-//                .and()
-//                .withInternal()
-//                .source(States.S1).target(States.S2).event(Events.E2);
+                .source(GenericState.IDLE).target(ArchivingState.MARK_ENTITIES).event(ArchivingEvent.ARCHIVING_START)
+                .and()
+                .withExternal()
+                .source(ArchivingState.MARK_ENTITIES).target(ArchivingState.TRANSFER)
+                .and()
+                .withExternal()
+                .source(ArchivingState.TRANSFER).target(ArchivingState.CLEANUP)
+                .and()
+                .withExternal()
+                .source(ArchivingState.CLEANUP).target(GenericState.IDLE);
+    }
+
+    @AllArgsConstructor
+    static class SimpleAction implements Action<DataManagementState, DataManagementEvent> {
+
+        private String actionName;
+
+
+        @Override
+        public void execute(StateContext<DataManagementState, DataManagementEvent> context) {
+            log.info("Simple Action '{}' performed", actionName);
+        }
     }
 
 }
--- src/main/java/com/github/szilex94/edu/round_tracker/service/datamanagement/fsm/event/ArchivingEvent.java
+++ src/main/java/com/github/szilex94/edu/round_tracker/service/datamanagement/fsm/event/ArchivingEvent.java
@@ -1,5 +1,5 @@
 package com.github.szilex94.edu.round_tracker.service.datamanagement.fsm.event;
 
 public enum ArchivingEvent implements DataManagementEvent {
-    ARCHIVING_START
+    ARCHIVING_START;
 }
