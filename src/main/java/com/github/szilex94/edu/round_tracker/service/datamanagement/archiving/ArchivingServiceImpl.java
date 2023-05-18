ccfd8e6f86b868d9864d295bee4433a629e47bb2
Mate Szilard <32964548+Szilex94@users.noreply.github.com>
2023-05-18 22:55:27 +0300

    RT-20 Updated ArchivingServiceImpl to properly handle event submission.

--- src/main/java/com/github/szilex94/edu/round_tracker/service/datamanagement/archiving/ArchivingServiceImpl.java
+++ src/main/java/com/github/szilex94/edu/round_tracker/service/datamanagement/archiving/ArchivingServiceImpl.java
@@ -2,6 +2,7 @@ package com.github.szilex94.edu.round_tracker.service.datamanagement.archiving;
 
 import com.github.szilex94.edu.round_tracker.service.datamanagement.statemachine.event.DataFlowEvent;
 import com.github.szilex94.edu.round_tracker.service.datamanagement.statemachine.state.DataManagementState;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.statemachine.StateMachine;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
+@Slf4j
 public class ArchivingServiceImpl implements ArchivingService {
 
     private final StateMachine<DataManagementState, DataFlowEvent> fsm;
@@ -23,12 +25,22 @@ public class ArchivingServiceImpl implements ArchivingService {
         Mono<Message<DataFlowEvent>> message = Mono.just(DataFlowEvent.ARCHIVING_START)
                 .map(GenericMessage::new);
         return fsm.sendEvent(message)
-                .then(Mono.just(new ArchivingSubmissionRequest()));
-//                .map(this::handleEventSubmission);
+                .singleOrEmpty()
+                .map(this::handleEventSubmission);
     }
 
     private ArchivingSubmissionRequest handleEventSubmission(StateMachineEventResult<DataManagementState, DataFlowEvent> submissionResult) {
-//TODO properly handle event submission
-        return new ArchivingSubmissionRequest();
+        var currentState = submissionResult.getRegion().getState().getId();
+        var type = submissionResult.getResultType();
+        switch (type) {
+            case ACCEPTED:
+            case DEFERRED:
+                return new ArchivingSubmissionRequest(true);
+            case DENIED:
+                log.debug("Archiving process start denied! Currently processing state {}", currentState);
+                return new ArchivingSubmissionRequest(false, "Currently processing  state: " + currentState);
+            default:
+                throw new IllegalStateException("Unknown state machine result type: " + type);
+        }
     }
 }
