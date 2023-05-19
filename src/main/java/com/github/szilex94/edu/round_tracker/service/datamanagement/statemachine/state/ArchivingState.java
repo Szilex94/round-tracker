78fae9805fb67014524bd6837b44672af599ce62
Mate Szilard <32964548+Szilex94@users.noreply.github.com>
2023-05-19 19:43:19 +0300

    RT-20 Updated javadoc

--- src/main/java/com/github/szilex94/edu/round_tracker/service/datamanagement/statemachine/state/ArchivingState.java
+++ src/main/java/com/github/szilex94/edu/round_tracker/service/datamanagement/statemachine/state/ArchivingState.java
@@ -1,8 +1,25 @@
 package com.github.szilex94.edu.round_tracker.service.datamanagement.statemachine.state;
 
+/**
+ * Represents the states used in the archiving process
+ *
+ * @author szilex94
+ */
 public enum ArchivingState implements DataManagementState {
+    /**
+     * In this stage entries which are older than the cut-off date are marked for archiving
+     */
     MARK_ENTITIES,
+    /**
+     * This stage represents the transfer of entities which where marked for archiving into the long term storage space
+     */
     TRANSFER,
+    /**
+     * This stage represents the cleanup process where any leftover information is removed
+     */
     CLEANUP,
+    /**
+     * Final stage of the archiving process where the transferred information is validated
+     */
     SANITY_CHECK
 }
